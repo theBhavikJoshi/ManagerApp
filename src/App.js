86582fa8bad825bf436348047bce331348a3fc6c
Bhavik Joshi <bhavikjoshi29@gmail.com>
2018-03-09 23:58:39 +0530

    Added Navigation Route Setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ReduxThunk from "redux-thunk";
 import firebase from "firebase";
 
 import reducers from "./reducers";
-import LoginForm from "./components/LoginForm";
+import Router from "./Router";
 
 class App extends Component {
   componentDidMount() {
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Stack, Scene, Router } from "react-native-router-flux";
+
+import LoginForm from "./components/LoginForm";
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Stack>
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Stack>
+    </Router>
+  );
+};
+
+export default RouterComponent;
